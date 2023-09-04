892cc13136be357e5f1121e13a159657d48c7eff
JoseCarlos653 <josecarlosperezreinosa@gmail.com>
2023-09-04 13:39:06 -0600

    Creando la tabla de la vista de usuarios con jQuey  DataTables y BootStrap

--- SistemaCarritoW/CapaPresentacionAdmin/Controllers/HomeController.cs
+++ SistemaCarritoW/CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -20,13 +20,14 @@ namespace CapaPresentacionAdmin.Controllers
             return View();
         }
 
+        [HttpGet]
         public JsonResult ListarUsuarios()
         {
             List<Usuario> oLista = new List<Usuario>();
 
             oLista = new CN_Usuarios().ListarUsuarios();
 
-            return Json(oLista, JsonRequestBehavior.AllowGet);
+            return Json(new {data = oLista }, JsonRequestBehavior.AllowGet);
         }
     }
 }
